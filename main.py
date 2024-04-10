7c1ac1e2677efc41147f0f61c855b5cdae0ea8c7
danielr3122 <drosenhamer@gmail.com>
2024-04-09 19:52:39 -0500

    Updated file recognition

--- main.py
+++ main.py
@@ -14,28 +14,16 @@ def extra_data_from_csv():
         file_path = input("Enter lidar data path (CSV):\n")
 
         while(not os.path.isfile(file_path)):
-            file_path = input("File path doesn't exist. Re-enter lidar data path (CSV):\n")
+            file_path = input("File path doesn't exist. Re-enter lidar data path (CSV format):\n")
+            
+        while(file_path[len(file_path)-3:len(file_path)].lower() != "csv"):
+            print(file_path[len(file_path)-3:len(file_path)])
+            file_path = input("Incorrect file type. Re-enter lidar data path (CSV format):\n")
 
-        if not os.path.isfile(file_path):
-            print("File path doesn't exist")
-
-        with open('2023-04-15_12-55-45.csv', newline='') as csvfile:
+        with open(file_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 raw_data.append([row['X'], row['Y'], row['Z'], row['Reflectivity']])
     print("Success")
 
-
-# def extra_data_from_csv():
-#     with open('test.csv', 'w', newline='') as newcsvfile:
-
-#         csvwriter = csv.writer(newcsvfile, delimiter=',',
-#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        
-#         file_path = input("Specify file path:\n")
-
-#         with open('2023-04-15_12-55-45.csv', newline='') as csvfile:
-#             reader = csv.DictReader(csvfile)
-#             for row in reader:
-#                 raw_data.append([row['X'], row['Y'], row['Z'], row['Reflectivity']])
-#     print("Success")
+extra_data_from_csv()
